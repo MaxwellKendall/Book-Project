dd73ed7c5104f236fd33b3772c3b947376ea6263
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-09-01 18:57:35 -0400

    after running webpack -p

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import App from './components/App';
 import rootReducer from './reducers/index';
+const gitHubRepoName = 'Book-Project';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)
