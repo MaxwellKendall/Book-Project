a01ca54b100a22f56bb0e3db4c7dbc94d23cb56f
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-10-09 15:20:04 -0400

    Updates

--- src/actions/books.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const selectBook = createAction('BOOK_SELECTED');
-export const newBook = createAction('NEW_BOOK');
-export const deleteBook = createAction('DELETE_BOOK');
-export const editBook = createAction('EDIT_BOOK');
